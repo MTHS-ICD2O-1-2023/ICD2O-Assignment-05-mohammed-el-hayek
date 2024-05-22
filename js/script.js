4105648abf0005726a4446ce1555b7611f8503d3
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-05-22 03:27:12 +0000

    V-A5.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculate() {
+  // get input value
+  const term = parseInt(document.getElementById("multiply-term").value)
+
+  // calculate first 5 terms
+  let answer = 1
+  let list = ""
+  for (let counter = 0; counter < 5; counter++) {
+    list = list + answer + ", "
+    answer = answer * term
+  }
+
+  // output
+  document.getElementById("answer").innerHTML = "The first 5 terms would be: " + list + "..."
+}
